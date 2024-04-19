efc2f718e155eedbe085e8f6c372ab3a2e19aa8e
lethalgem <iwc123@gmail.com>
2024-04-19 00:53:53 -0500

    remove ore when off screen and stabilize camera

--- scripts/ore.gd
+++ scripts/ore.gd
@@ -10,7 +10,7 @@ var oreId = 0
 @onready var current_velocity = 300 * _speedModifier
 const minSpeed = 250
 
-var createdPlanet = null
+var createdPlanet: Planet  = null
 func setCreatePlanet(planet, level_id):
 	_level_id = level_id
 	if _level_id == 1:
@@ -61,19 +61,19 @@ func setBeenDetected():
 func forceDirection(direction:Vector2):
 	current_direction = direction.normalized()
 
-#func _physics_process(delta):
-func _process(delta):
+func _physics_process(delta):
 	var sortedKeys = _gravitySources.keys()
 	sortedKeys.sort()
 	for blackHole in sortedKeys:
-		#print(blackHole)
 		var direction_from_ore_to_blackHole = self.global_position.direction_to(_gravitySources[blackHole][0])
 		var distance_to_ore = self.global_position.distance_to(_gravitySources[blackHole][0])
 		update(direction_from_ore_to_blackHole, distance_to_ore, blackHole.getCurrentGravity(), delta) #_gravitySources[blackHole][1], delta)
 
 	var new_position = global_position + (current_direction * current_velocity * delta)
-	#create_tween().tween_property(self, "global_position", new_position, delta)
 	global_position = new_position
+
+	if global_position.x < -8000 or global_position.x > 11490 or global_position.y < -18300 or global_position.y > 1455:
+		createdPlanet._level.deleteOre(self)
 
 @onready var _inGravityBubble = false
 func enteredGravityBubble():
@@ -98,18 +98,15 @@ func removeGravitySource(blackHole:BlackHole):
 
 func update(direction:Vector2, distance:float, gravity:float, delta:float):
 
-	#if distance > 700 or _inGravityBubble:
 	if distance > 500 or _inGravityBubble:
 		return
 
 	var impactVector = pow(_speedModifier, 2.0/3) * direction * gravity * delta / (pow(distance, 1.5))
-	#var impactVector = _speedModifier * direction * gravity * delta / (pow(distance, 1.5))
+
 	var newVector = (current_direction * current_velocity) + impactVector
 
 	var originalVelocity = current_velocity
 	current_velocity = newVector.length()
-	#current_velocity = min(originalVelocity * (1.05 + (pow(1.05, _speedModifier) - 1.05) / 3), current_velocity)
-	#current_velocity = max(originalVelocity * (.95 - (.95 - pow(.95, _speedModifier)) / 3), current_velocity)
 	current_velocity = min(originalVelocity * 1.05, current_velocity)
 	current_velocity = max(originalVelocity * .95, current_velocity)
 	current_velocity = max(minSpeed, current_velocity)
